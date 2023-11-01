c61c0e4d9c46177409ccdb79a76b33cef2d5cf2e
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2023-11-01 15:51:10 +0600

    fix: Metform hooks issue fixed

--- includes/Triggers/Met/Hooks.php
+++ includes/Triggers/Met/Hooks.php
@@ -6,4 +6,5 @@ if (!defined('ABSPATH')) {
 use BitCode\FI\Core\Util\Hooks;
 use BitCode\FI\Triggers\Met\MetController;
 
-Hooks::add('metform_pro_form_data_for_pro_integrations', [MetController::class, 'handle_metform_submit'], 10, 3);
+Hooks::add('metform_pro_form_data_for_pro_integrations', [MetController::class, 'handle_metform_pro_submit'], 10, 3);
+Hooks::add('metform_after_store_form_data', [MetController::class, 'handle_metform_submit'], 10, 3);
