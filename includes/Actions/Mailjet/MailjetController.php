3aae57515c5379f3fb424762e40a9de5a56b012a
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-02-21 16:16:34 +0600

    fix: Mailjet Contact list limitation issue fixed

--- includes/Actions/Mailjet/MailjetController.php
+++ includes/Actions/Mailjet/MailjetController.php
@@ -20,7 +20,7 @@ class MailjetController
             wp_send_json_error(__('Requested parameter is empty', 'bit-integrations'), 400);
         }
 
-        $apiEndpoints = 'https://api.mailjet.com/v3/REST/contactslist/';
+        $apiEndpoints = 'https://api.mailjet.com/v3/REST/contactslist?Limit=1000';
         $apiKey       = $fieldsRequestParams->apiKey;
         $secretKey    = $fieldsRequestParams->secretKey;
         $header       = [
