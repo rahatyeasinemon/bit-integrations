dd0da2222aec69431726c3f6ba15d7deff6c514b
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-03-24 15:30:49 +0600

    feat: Elementor routes created

--- includes/Triggers/Breakdance/Routes.php
+++ includes/Triggers/Breakdance/Routes.php
@@ -7,8 +7,6 @@ if (!defined('ABSPATH')) {
 use BitCode\FI\Core\Util\Route;
 use BitCode\FI\Triggers\Breakdance\BreakdanceController;
 
-// Route::get('breakdance/get', [BreakdanceController::class, 'getAllForms']);
-// Route::post('breakdance/get/form', [BreakdanceController::class, 'getFormFields']);
 Route::post('breakdance/test', [BreakdanceController::class, 'getTestData']);
 Route::post('breakdance/test/remove', [BreakdanceController::class, 'removeTestData']);
 
