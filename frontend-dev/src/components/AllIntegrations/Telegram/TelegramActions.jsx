5310a8daba5912930731dd30e73ef45d21ebfb8b
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-03-27 14:43:58 +0600

    fix: Telegram attachment action sub title fixed

--- frontend-dev/src/components/AllIntegrations/Telegram/TelegramActions.jsx
+++ frontend-dev/src/components/AllIntegrations/Telegram/TelegramActions.jsx
@@ -9,7 +9,7 @@ import TableCheckBox from '../../Utilities/TableCheckBox'
 export default function TelegramActions({ formFields, telegramConf, setTelegramConf }) {
   const [actionMdl, setActionMdl] = useState({ show: false })
   const actionHandler = (e) => {
-    const newConf =  deepCopy(telegramConf)
+    const newConf = deepCopy(telegramConf)
     if (e.target.value !== '') {
       newConf.actions.attachments = e.target.value
     } else {
@@ -20,7 +20,7 @@ export default function TelegramActions({ formFields, telegramConf, setTelegramC
   return (
     <div className="pos-rel">
       <div className="d-flx flx-wrp">
-        <TableCheckBox onChange={() => setActionMdl({ show: 'attachments' })} checked={'attachments' in telegramConf.actions} className="wdt-200 mt-4 mr-2" value="Attachment" title={__('Attachments', 'bit-integrations')} subTitle={__('Add attachments from BitForm to send Telegram.', 'bit-integrations')} />
+        <TableCheckBox onChange={() => setActionMdl({ show: 'attachments' })} checked={'attachments' in telegramConf.actions} className="wdt-200 mt-4 mr-2" value="Attachment" title={__('Attachments', 'bit-integrations')} subTitle={__('Add attachments from Bit Integrations to send Telegram.', 'bit-integrations')} />
       </div>
 
       <ConfirmModal
