4f079f4047bc1e61d5c22eaace65848d60e92c24
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-04-03 14:47:23 +0600

    feat: Keap custom_field route added

--- includes/Actions/Keap/Routes.php
+++ includes/Actions/Keap/Routes.php
@@ -4,10 +4,9 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-use BitCode\FI\Actions\Keap\KeapController;
 use BitCode\FI\Core\Util\Route;
+use BitCode\FI\Actions\Keap\KeapController;
 
-Route::post('keap_generate_token', [ KeapController::class, 'generateTokens']);
-// Route::post('keap_refresh_audience', [ KeapController::class, 'refreshAudience']);
-// Route::post('keap_refresh_fields', [ KeapController::class, 'refreshAudienceFields']);
-Route::post('keap_fetch_all_tags', [ KeapController::class, 'refreshTagListAjaxHelper']);
+Route::post('keap_generate_token', [KeapController::class, 'generateTokens']);
+Route::post('keap_fetch_all_tags', [KeapController::class, 'refreshTagListAjaxHelper']);
+Route::post('keap_fetch_all_custom_fields', [KeapController::class, 'refreshCustomFieldAjaxHelper']);
