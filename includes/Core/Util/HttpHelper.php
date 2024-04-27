8613c1e78af968ee41618bd3596eb74b7b97e4bb
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-04-27 12:26:06 +0600

    refactor: HttpHelper refactored & retrieved response Code code

--- includes/Core/Util/HttpHelper.php
+++ includes/Core/Util/HttpHelper.php
@@ -4,57 +4,26 @@ namespace BitCode\FI\Core\Util;
 
 final class HttpHelper
 {
+    public static $responseCode = null;
+
     public static function post($url, $data, $headers = null, $options = null)
     {
-        $defaultOptions =[
-            'headers' => $headers,
-            'body' => $data,
-            "timeout" => 30
-        ];
-
-        $options = wp_parse_args($options, $defaultOptions);
-        $requestReponse = wp_remote_post($url, $options);
-
-        if (is_wp_error($requestReponse)) {
-            return $requestReponse;
-        }
-        // $responseCode = wp_remote_retrieve_response_code($requestReponse);
-        // if (!\is_null($responseCode) && $responseCode != 200) {
-        //     return wp_remote_retrieve_response_message($requestReponse);
-        // }
-        $responseBody = wp_remote_retrieve_body($requestReponse);
-        $jsonData = json_decode($responseBody);
-        return \is_null($jsonData) ? $responseBody : $jsonData;
+        return static::request($url, 'POST', $data, $headers, $options);
     }
 
     public static function get($url, $data, $headers = null, $options = null)
     {
-        $defaultOptions =[
-            'headers' => $headers,
-            'body' => $data,
-            "timeout" => 30
-        ];
-        $options = wp_parse_args($options, $defaultOptions);
-        $requestReponse = wp_remote_get($url, $options);
-        if (is_wp_error($requestReponse)) {
-            return $requestReponse;
-        }
-        // $responseCode = wp_remote_retrieve_response_code($requestReponse);
-        // if (!\is_null($responseCode) && $responseCode != 200) {
-        //     return wp_remote_retrieve_response_message($requestReponse);
-        // }
-        $responseBody = wp_remote_retrieve_body($requestReponse);
-        $jsonData = json_decode($responseBody);
-        return \is_null($jsonData) ? $responseBody : $jsonData;
+        return static::request($url, 'GET', $data, $headers, $options);
     }
 
     public static function request($url, $type, $data, $headers = null, $options = null)
     {
-        $defaultOptions =[
-            'method' => $type,
-            'headers' => $headers,
-            'body' => $data,
-            "timeout" => 30
+        static::$responseCode = null;
+        $defaultOptions = [
+            'method'    => $type,
+            'headers'   => $headers,
+            'body'      => $data,
+            "timeout"   => 30
         ];
         $options = wp_parse_args($options, $defaultOptions);
         $requestReponse = wp_remote_request($url, $options);
@@ -65,8 +34,10 @@ final class HttpHelper
         // if (!\is_null($responseCode) && $responseCode != 200) {
         //     return wp_remote_retrieve_response_message($requestReponse);
         // }
-        $responseBody = wp_remote_retrieve_body($requestReponse);
-        $jsonData = json_decode($responseBody);
+        static::$responseCode   = wp_remote_retrieve_response_code($requestReponse);
+        $responseBody           = wp_remote_retrieve_body($requestReponse);
+        $jsonData               = json_decode($responseBody);
+
         return \is_null($jsonData) ? $responseBody : $jsonData;
     }
 }
