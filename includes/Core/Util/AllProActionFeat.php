c8edcc67f5065d3904cdfd42fbcbc6563bb7c1df
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-07-11 15:12:23 +0600

    refactor: Mailchimp pro module feature listed

--- includes/Core/Util/AllProActionFeat.php
+++ includes/Core/Util/AllProActionFeat.php
@@ -6,8 +6,11 @@ namespace BitCode\FI\Core\Util;
 final class AllProActionFeat
 {
     public static $features = [
+        'MailChimp' => [
+            ['feat_name' => 'addRemoveTag', 'class' => '\BitApps\BTCBI_PRO\Actions\MailChimp\MailChimpRecordHelper', 'pro_init_v' => '2.0.9']
+        ],
         'FreshSales' => [
             ['feat_name' => 'upsertRecord', 'class' => '\BitApps\BTCBI_PRO\Actions\FreshSales\FreshSalesRecordApiHelper', 'pro_init_v' => '2.1.1']
-        ],
+        ]
     ];
 }
