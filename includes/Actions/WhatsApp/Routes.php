4812bfce16377d6b77547a40ad1aebe48b87f638
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-07-31 16:32:24 +0600

    feat: WhatsApp Templets routes created

--- includes/Actions/WhatsApp/Routes.php
+++ includes/Actions/WhatsApp/Routes.php
@@ -8,3 +8,4 @@ use BitCode\FI\Actions\WhatsApp\WhatsAppController;
 use BitCode\FI\Core\Util\Route;
 
 Route::post('whats_app_authorization', [WhatsAppController::class, 'Authorization']);
+Route::post('whats_app_all_template', [WhatsAppController::class, 'getAllTemplate']);
