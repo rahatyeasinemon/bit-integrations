1fe9755d0d454d0f32869ffec83c73a3ddd838a4
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-08-06 18:22:54 +0600

    feat: WhatsApp Media execution added

--- includes/Actions/WhatsApp/RecordApiHelper.php
+++ includes/Actions/WhatsApp/RecordApiHelper.php
@@ -8,6 +8,7 @@ namespace BitCode\FI\Actions\WhatsApp;
 
 use BitCode\FI\Log\LogHandler;
 use BitCode\FI\Core\Util\Common;
+use BitCode\FI\Core\Util\Helper;
 use BitCode\FI\Core\Util\HttpHelper;
 
 /**
@@ -32,12 +33,12 @@ class RecordApiHelper
         $businessAccountID,
         $token,
         $data,
-        $templateName,
         $phoneNumber
     ) {
         $apiEndPoint = "{$this->_baseUrl}{$businessAccountID}/message_templates?fields=language&name={$templateName}";
         $response = HttpHelper::get($apiEndPoint, null, static::setHeaders($token));
         $language = $response->data[0]->language ?? 'en_US';
+        $templateName = $this->_integrationDetails->templateName;
 
         $apiEndPoint = "{$this->_baseUrl}{$numberId}/messages";
         $data = [
@@ -61,14 +62,38 @@ class RecordApiHelper
         $token,
         $phoneNumber
     ) {
-        $textBody = $this->_integrationDetails->body;
-        $response = apply_filters('btcbi_whatsapp_send_text_messages', $textBody, $fieldValues, $numberId, $token, $phoneNumber);
+        if (Helper::proActionFeatExists('WhatsApp', 'sendTextMessages')) {
+            $textBody = $this->_integrationDetails->body;
+            $response = apply_filters('btcbi_whatsapp_send_text_messages', $textBody, $fieldValues, $numberId, $token, $phoneNumber);
 
-        if (isset($response->messages[0]->id) || isset($response->error) || is_wp_error($response)) {
-            return $response;
-        } else {
+            if (isset($response->messages[0]->id) || isset($response->error) || is_wp_error($response)) {
+                return $response;
+            }
+
             return (object) ['error' => 'Bit Integration Pro plugin is not installed or activate'];
         }
+
+        return (object) ['error' => 'Bit Integration Pro plugin is not installed or activate'];
+    }
+
+    public function sendMessageWithMedia(
+        $numberId,
+        $fieldValues,
+        $token,
+        $phoneNumber
+    ) {
+        if (Helper::proActionFeatExists('WhatsApp', 'sendMediaMessages')) {
+            error_log(print_r($this->_integrationDetails, true));
+            $response = apply_filters('btcbi_whatsapp_send_media_messages', $this->_integrationDetails, $fieldValues, $numberId, $token, $phoneNumber);
+
+            if (isset($response->messages[0]->id) || isset($response->error) || is_wp_error($response)) {
+                return $response;
+            }
+            
+            return (object) ['error' => 'Bit Integration Pro plugin is not installed or activate'];
+        }
+
+        return (object) ['error' => 'Bit Integration Pro plugin is not installed or activate'];
     }
 
     public function generateReqDataFromFieldMap($data, $fieldMap)
@@ -93,16 +118,19 @@ class RecordApiHelper
         $fieldMap = $this->_integrationDetails->field_map;
         $finalData = $this->generateReqDataFromFieldMap($fieldValues, $fieldMap);
         $phoneNumber = ltrim($finalData['phone'], '+');
-        $templateName = $this->_integrationDetails->templateName;
 
         $numberId = $this->_integrationDetails->numberID;
         $businessAccountID = $this->_integrationDetails->businessAccountID;
         $token = $this->_integrationDetails->token;
 
         if ($messageType === 'template' || $messageType === '2') {
-            $apiResponse = $this->sendMessageWithTemplate($numberId, $businessAccountID, $token, $finalData, $templateName, $phoneNumber);
+            $templateName = $this->_integrationDetails->templateName;
+            $apiResponse = $this->sendMessageWithTemplate($numberId, $businessAccountID, $token, $finalData, $phoneNumber);
         } elseif ($messageType === 'text') {
             $apiResponse = $this->sendMessageWithText($numberId, $fieldValues, $token, $phoneNumber);
+        } elseif ($messageType === 'media') {
+            $apiResponse = $this->sendMessageWithMedia($numberId, $fieldValues, $token, $phoneNumber);
+            die;
         }
 
         if (property_exists($apiResponse, 'error')) {
