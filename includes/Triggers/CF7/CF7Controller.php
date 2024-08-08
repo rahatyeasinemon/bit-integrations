480285b5a83a41d7b9d6e64ac403aa250c3cc1a7
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-08-08 12:58:33 +0600

    feat: Contact Form 7 custom html fields added

--- includes/Triggers/CF7/CF7Controller.php
+++ includes/Triggers/CF7/CF7Controller.php
@@ -87,7 +87,7 @@ final class CF7Controller
             return $fieldDetails;
         }
 
-        $fields = [];
+        $fields = static::getCustomHtmlFields($form_text);
 
         foreach ($fieldDetails as $field) {
             if (!empty($field->name) && $field->type !== 'submit') {
@@ -97,6 +97,31 @@ final class CF7Controller
                     'label' => $field->name,
                 ];
             }
+        }
+
+        return $fields;
+    }
+
+    private static function getCustomHtmlFields($form_text)
+    {
+        $fields = [];
+
+        preg_match_all('/<input[^>]*name="([^"]*)"[^>]*type="([^"]*)"|<input[^>]*type="([^"]*)"[^>]*name="([^"]*)"/is', $form_text, $inputMatches);
+        foreach ($inputMatches[0] as $index => $input) {
+            $name = !empty($inputMatches[1][$index]) ? $inputMatches[1][$index] : $inputMatches[4][$index];
+            $type = !empty($inputMatches[2][$index]) ? $inputMatches[2][$index] : $inputMatches[3][$index];
+
+            $fields[] = ['name'  => $name, 'type'  => $type, 'label' => $name];
+        }
+
+        preg_match_all('/<select[^>]*name="([^"]*)"[^>]*>(.*?)<\/select>/is', $form_text, $selectMatches);
+        foreach ($selectMatches[1] as $name) {
+            $fields[] = ['name'  => $name, 'type'  => 'select', 'label' => $name];
+        }
+
+        preg_match_all('/<textarea[^>]*name="([^"]*)"[^>]*>(.*?)<\/textarea>/is', $form_text, $textareaMatches);
+        foreach ($textareaMatches[1] as $name) {
+            $fields[] = ['name'  => $name, 'type'  => 'textarea', 'label' => $name];
         }
 
         return $fields;
