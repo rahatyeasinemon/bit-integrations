00aaa3d6c34e96184b826158c53134614f3f79fa
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-08-20 16:12:12 +0600

    remove: Brizy entity conditions

--- frontend-dev/src/components/AllIntegrations/IntegrationHelpers/IntegrationHelpers.js
+++ frontend-dev/src/components/AllIntegrations/IntegrationHelpers/IntegrationHelpers.js
@@ -68,11 +68,7 @@ export const saveIntegConfig = async (
    * Have to move this to a better place
    */
 
-  if (
-    flow.triggered_entity === 'Bricks' ||
-    flow.triggered_entity === 'Brizy' ||
-    flow.triggered_entity === 'CartFlow'
-  ) {
+  if (flow.triggered_entity === 'Bricks' || flow.triggered_entity === 'CartFlow') {
     if (edit) {
       tmpConf.postId = flow?.flow_details?.postId ?? null
     } else {
@@ -254,11 +250,7 @@ export const saveActionConf = async ({
    * This is a temporary fix
    * Have to move this to a better place
    */
-  if (
-    flow.triggered_entity === 'Bricks' ||
-    flow.triggered_entity === 'Brizy' ||
-    flow.triggered_entity === 'CartFlow'
-  ) {
+  if (flow.triggered_entity === 'Bricks' || flow.triggered_entity === 'CartFlow') {
     if (edit) {
       tmpConf.postId = flow?.flow_details?.postId ?? null
     } else {
