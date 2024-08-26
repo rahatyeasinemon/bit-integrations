773f4832a0500f0ef508b8050de604ef456f125c
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-08-26 17:52:34 +0600

    core: ActionHook moved into Pro

--- includes/Triggers/ActionHook/ActionHookController.php
+++ includes/Triggers/ActionHook/ActionHookController.php
@@ -2,90 +2,12 @@
 
 namespace BitCode\FI\Triggers\ActionHook;
 
-use WP_Error;
+use ReflectionClass;
 use BitCode\FI\Flow\Flow;
 use BitCode\FI\Core\Util\Helper;
 
 class ActionHookController
 {
-    public static function info()
-    {
-        return [
-            'name'              => 'Action Hook',
-            'title'             => 'Get callback data through an URL',
-            'type'              => 'action_hook',
-            'is_active'         => true,
-            'documentation_url' => 'https://bitapps.pro/docs/bit-integrations/trigger/action-hook-integrations',
-            'tutorial_url'      => 'https://youtu.be/pZ-8JuZfIco?si=Xxv857hJjv6p5Tcu',
-            'fetch'             => [
-                'action' => 'elementor/test',
-                'method' => 'post',
-            ],
-            'fetch_remove' => [
-                'action' => 'elementor/test/remove',
-                'method' => 'post',
-            ],
-            'isPro' => false
-        ];
-    }
-
-    public function getTestData($data)
-    {
-        $missing_field = null;
-
-        if (!property_exists($data, 'hook_id')) {
-            $missing_field = \is_null($missing_field) ? 'ActionHook ID' : $missing_field . ', ActionHook ID';
-        }
-        if (!\is_null($missing_field)) {
-            wp_send_json_error(\sprintf(__('%s can\'t be empty or need to be valid', 'bit-integrations'), $missing_field));
-        }
-
-        $testData = get_option('btcbi_action_hook_test_' . $data->hook_id);
-        if ($testData === false) {
-            update_option('btcbi_action_hook_test_' . $data->hook_id, []);
-        }
-        if (!$testData || empty($testData)) {
-            wp_send_json_error(new WP_Error('actionHook_test', __('ActionHook data is empty', 'bit-integrations')));
-        }
-        wp_send_json_success(['actionHook' => $testData]);
-    }
-
-    public static function actionHookHandler(...$args)
-    {
-        $args = ActionHookHelper::convertToSimpleArray($args);
-
-        if (get_option('btcbi_action_hook_test_' . current_action()) !== false) {
-            update_option('btcbi_action_hook_test_' . current_action(), $args);
-        }
-
-        return rest_ensure_response(['status' => 'success']);
-    }
-
-    public function removeTestData($data)
-    {
-        $missing_field = null;
-        if (!property_exists($data, 'hook_id') && !empty($data->hook_id)) {
-            $missing_field = \is_null($missing_field) ? 'ActionHook ID' : $missing_field . ', ActionHook ID';
-        }
-        if (!\is_null($missing_field)) {
-            wp_send_json_error(\sprintf(__('%s can\'t be empty or need to be valid', 'bit-integrations'), $missing_field));
-        }
-
-        if (property_exists($data, 'reset') && $data->reset) {
-            $testData = update_option('btcbi_action_hook_test_' . $data->hook_id, []);
-        } elseif ($data->hook_id == 'all') {
-            global $wpdb;
-            $testData = $wpdb->get_results($wpdb->prepare("DELETE FROM {$wpdb->options} WHERE option_name LIKE '%btcbi_action_hook_test_%'"));
-        } else {
-            $testData = delete_option('btcbi_action_hook_test_' . $data->hook_id);
-        }
-
-        if (!$testData) {
-            wp_send_json_error(new WP_Error('actionHook_test', __('Failed to remove test data', 'bit-integrations')));
-        }
-        wp_send_json_success(__('ActionHook test data removed successfully', 'bit-integrations'));
-    }
-
     public static function handle(...$args)
     {
         if ($flows = Flow::exists('ActionHook', current_action())) {
@@ -96,7 +18,7 @@ class ActionHookController
                     continue;
                 }
 
-                $args = ActionHookHelper::convertToSimpleArray($args);
+                $args = static::convertToSimpleArray($args);
                 $primaryKeyValue = Helper::extractValueFromPath($args, $flowDetails->primaryKey->key, 'ActionHook');
 
                 if ($flowDetails->primaryKey->value === $primaryKeyValue) {
@@ -123,5 +45,39 @@ class ActionHookController
         }
 
         return rest_ensure_response(['status' => 'success']);
+    }
+
+    private static function convertToSimpleArray($value)
+    {
+        if (\is_object($value)) {
+            $value = static::convertObjectToArray($value);
+        }
+
+        if (\is_array($value)) {
+            foreach ($value as $key => $subValue) {
+                $value[$key] = static::convertToSimpleArray($subValue);
+            }
+        }
+
+        return $value;
+    }
+
+    private static function convertObjectToArray($object)
+    {
+        $reflection = new ReflectionClass($object);
+        $properties = $reflection->getProperties();
+
+        $array = [];
+        foreach ($properties as $property) {
+            $property->setAccessible(true);
+            $name = $property->getName();
+            $value = $property->getValue($object);
+
+            $name = preg_replace('/^\x00(?:\*\x00|\w+\x00)/', '', $name);
+
+            $array[$name] = static::convertToSimpleArray($value);
+        }
+
+        return $array;
     }
 }
