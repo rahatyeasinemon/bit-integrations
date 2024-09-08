2119ef9d34028e60682d02953f05b8a6538673a2
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-09-08 11:52:22 +0600

    feat: userRegistration req notReq fields added

--- frontend-dev/src/Utils/StaticData/userField.js
+++ frontend-dev/src/Utils/StaticData/userField.js
@@ -3,38 +3,46 @@ export const userFields = [
   {
     key: 'user_email',
     name: 'Email',
-    required: true,
+    required: true
   },
   {
     key: 'user_login',
     name: 'Username',
+    required: false
   },
   {
     key: 'nickname',
     name: 'Nickname',
+    required: false
   },
   {
     key: 'display_name',
     name: 'Display Name',
+    required: false
   },
   {
     key: 'first_name',
     name: 'First Name',
+    required: false
   },
   {
     key: 'last_name',
     name: 'Last Name',
+    required: false
   },
   {
     key: 'user_pass',
     name: 'Password',
+    required: false
   },
   {
     key: 'user_url',
     name: 'Website',
+    required: false
   },
   {
     key: 'description',
     name: 'Biographical Info',
-  },
+    required: false
+  }
 ]
