afc26e4e4bb31bfdae348a9aa610ea00fa558c86
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-10-05 15:28:17 +0600

    feat: PipeDrive Organization module utilities added

--- frontend-dev/src/components/AllIntegrations/PipeDrive/PipeDriveActions.jsx
+++ frontend-dev/src/components/AllIntegrations/PipeDrive/PipeDriveActions.jsx
@@ -321,7 +321,7 @@ export default function PipeDriveActions({
             />
           </>
         )}
-        {['Leads', 'Deals', 'Persons', 'Products'].includes(module) && (
+        {['Leads', 'Deals', 'Persons', 'Products', 'Organization'].includes(module) && (
           <TableCheckBox
             checked={pipeDriveConf.actions?.visible_to || false}
             onChange={(e) => actionHandler(e, 'visible_to')}
