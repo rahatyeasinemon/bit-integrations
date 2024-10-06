c9463a0c7a0143aec42b7ddb612ee52a505f5eea
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-10-06 16:48:59 +0600

    refactor: Pipedrive related list pro validation

--- frontend-dev/src/components/AllIntegrations/PipeDrive/PipeDriveIntegLayout.jsx
+++ frontend-dev/src/components/AllIntegrations/PipeDrive/PipeDriveIntegLayout.jsx
@@ -4,7 +4,18 @@ import PipeDriveNewRecord from './PipeDriveNewRecord'
 import PipeDriveRelatedRecord from './PipeDriveRelatedRecord'
 import CloseIcn from '../../../Icons/CloseIcn'
 
-export default function PipeDriveIntegLayout({ tab, settab, formID, formFields, handleInput, pipeDriveConf, setPipeDriveConf, isLoading, setIsLoading, setSnackbar }) {
+export default function PipeDriveIntegLayout({
+  tab,
+  settab,
+  formID,
+  formFields,
+  handleInput,
+  pipeDriveConf,
+  setPipeDriveConf,
+  isLoading,
+  setIsLoading,
+  setSnackbar
+}) {
   const addNewRelatedTab = () => {
     if (pipeDriveConf.relatedlists.length < 2) {
       const newConf = { ...pipeDriveConf }
@@ -12,13 +23,13 @@ export default function PipeDriveIntegLayout({ tab, settab, formID, formFields, 
         actions: {},
         field_map: [{ formField: '', pipeDriveFormField: '' }],
         moduleData: {},
-        module: '',
+        module: ''
       })
       setPipeDriveConf({ ...newConf })
     }
   }
 
-  const removeRelatedTab = indx => {
+  const removeRelatedTab = (indx) => {
     const newConf = { ...pipeDriveConf }
     newConf.relatedlists.splice(indx, 1)
 
@@ -30,15 +41,18 @@ export default function PipeDriveIntegLayout({ tab, settab, formID, formFields, 
     <>
       <br />
       <b className="wdt-200 d-in-b">{__('Module:', 'bit-integrations')}</b>
-      <select onChange={handleInput} name="module" value={pipeDriveConf.moduleData.module} className="btcd-paper-inp w-5" disabled={tab}>
+      <select
+        onChange={handleInput}
+        name="module"
+        value={pipeDriveConf.moduleData.module}
+        className="btcd-paper-inp w-5"
+        disabled={tab}>
         <option value="">{__('Select Module', 'bit-integrations')}</option>
-        {
-          Object.keys(pipeDriveConf.default.modules).map(moduleApiName => (
-            <option key={moduleApiName} value={moduleApiName}>
-              {moduleApiName}
-            </option>
-          ))
-        }
+        {Object.keys(pipeDriveConf.default.modules).map((moduleApiName) => (
+          <option key={moduleApiName} value={moduleApiName}>
+            {moduleApiName}
+          </option>
+        ))}
       </select>
       <br />
       <div>
@@ -50,18 +64,37 @@ export default function PipeDriveIntegLayout({ tab, settab, formID, formFields, 
               </button>
             </Tab>
 
-            {pipeDriveConf?.relatedlists && pipeDriveConf.relatedlists.map((_, indx) => (
-              <>
-                <Tab key={`t-${indx * 3}`}>
-                  <button className={`btcd-s-tab-link ${tab === indx + 1 && 's-t-l-active'}`} type="button">
-                    {__('Related List #', 'bit-integrations')}
-                    {indx + 1}
+            {pipeDriveConf?.relatedlists &&
+              pipeDriveConf.relatedlists.map((_, indx) => (
+                <>
+                  <Tab key={`t-${indx * 3}`}>
+                    <button
+                      className={`btcd-s-tab-link ${tab === indx + 1 && 's-t-l-active'}`}
+                      type="button">
+                      {__('Related List', 'bit-integrations')} #{indx + 1}
+                    </button>
+                  </Tab>
+                  <button
+                    onClick={() => removeRelatedTab(indx)}
+                    className="icn-btn"
+                    aria-label="delete-relatedlist"
+                    type="button">
+                    <CloseIcn size="14" />
                   </button>
-                </Tab>
-                <button onClick={() => removeRelatedTab(indx)} className="icn-btn" aria-label="delete-relatedlist" type="button"><CloseIcn size="14" /></button>
-              </>
-            ))}
-            {pipeDriveConf.relatedlists.length < 2 && pipeDriveConf.default.modules[pipeDriveConf.moduleData.module]?.relatedlists && <button onClick={addNewRelatedTab} className="icn-btn sh-sm ml-2 mr-2 tooltip" style={{ '--tooltip-txt': `'${__('Add More Related List', 'bit-integrations')}'` }} type="button">+</button>}
+                </>
+              ))}
+            {pipeDriveConf.relatedlists.length < 2 &&
+              pipeDriveConf.default.modules[pipeDriveConf.moduleData.module]?.relatedlists && (
+                <button
+                  onClick={addNewRelatedTab}
+                  className="icn-btn sh-sm ml-2 mr-2 tooltip"
+                  style={{
+                    '--tooltip-txt': `'${__('Add More Related List', 'bit-integrations')}'`
+                  }}
+                  type="button">
+                  +
+                </button>
+              )}
           </div>
           <div className="btcd-hr" />
 
@@ -78,8 +111,8 @@ export default function PipeDriveIntegLayout({ tab, settab, formID, formFields, 
               setSnackbar={setSnackbar}
             />
           </Panel>
-          {
-            pipeDriveConf?.relatedlists && pipeDriveConf.relatedlists.map((_, indx) => (
+          {pipeDriveConf?.relatedlists &&
+            pipeDriveConf.relatedlists.map((_, indx) => (
               <Panel key={`p-${indx + 2.4}`}>
                 <PipeDriveRelatedRecord
                   indx={indx}
@@ -95,12 +128,10 @@ export default function PipeDriveIntegLayout({ tab, settab, formID, formFields, 
                   setSnackbar={setSnackbar}
                 />
               </Panel>
-            ))
-          }
+            ))}
         </Tabs>
       </div>
       <br />
-
     </>
   )
 }
--- frontend-dev/src/components/AllIntegrations/PipeDrive/PipeDriveRelatedRecord.jsx
+++ frontend-dev/src/components/AllIntegrations/PipeDrive/PipeDriveRelatedRecord.jsx
@@ -5,6 +5,8 @@ import { addFieldMap } from '../IntegrationHelpers/IntegrationHelpers'
 import PipeDriveActions from './PipeDriveActions'
 import { handleTabChange } from './PipeDriveCommonFunc'
 import PipeDriveFieldMap from './PipeDriveFieldMap'
+import { useRecoilValue } from 'recoil'
+import { $btcbi } from '../../../GlobalStates'
 
 export default function PipeDriveRelatedRecord({
   indx,
@@ -19,6 +21,9 @@ export default function PipeDriveRelatedRecord({
   setIsLoading,
   setSnackbar
 }) {
+  const btcbi = useRecoilValue($btcbi)
+  const { isPro } = btcbi
+
   useEffect(() => {
     handleTabChange(
       indx + 1,
@@ -32,7 +37,7 @@ export default function PipeDriveRelatedRecord({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   // eslint-disable-next-line no-undef
-  const isPro = typeof btcbi !== 'undefined' && btcbi.isPro
+
   return (
     <>
       {isLoading && (
@@ -48,98 +53,101 @@ export default function PipeDriveRelatedRecord({
       )}
       <br />
       <br />
-      <div className="pos-rel">
-        {!isPro && (
-          <div className="pro-blur flx w-9">
+      {!isPro && (
+        <div className="pos-rel">
+          <div className="pro-blur flx p-3">
             <div className="pro">
-              {__('Available On', 'bit-integrations')}
-              <a href="https://bitapps.pro/" target="_blank" rel="noreferrer">
-                <span className="txt-pro"> {__('Premium', 'bit-integrations')}</span>
+              <a
+                href="https://bitapps.pro/bit-integrations/#pricing"
+                target="_blank"
+                rel="noreferrer">
+                {__('Related List', 'bit-integrations')} {__('Available On', 'bit-integrations')}
+                <span className="txt-pro"> ({__('Pro', 'bit-integrations')})</span>
               </a>
             </div>
           </div>
-        )}
-        <b className="wdt-100 d-in-b">{__('Related List:', 'bit-integrations')}</b>
-        <select
-          onChange={handleInput}
-          name="module"
-          value={pipeDriveConf?.relatedlists?.[tab - 1]?.module}
-          className="btcd-paper-inp w-7"
-          disabled={!pipeDriveConf.moduleData?.module}>
-          <option value="">{__('Select Related Module', 'bit-integrations')}</option>
-          {pipeDriveConf.default.modules[pipeDriveConf.moduleData.module]?.relatedlists?.map(
-            (relatedlistApiName) => (
-              <option key={relatedlistApiName.name} value={relatedlistApiName.name}>
-                {relatedlistApiName.name}
-              </option>
-            )
+          <b className="wdt-100 d-in-b">{__('Related List:', 'bit-integrations')}</b>
+          <select
+            onChange={handleInput}
+            name="module"
+            value={pipeDriveConf?.relatedlists?.[tab - 1]?.module}
+            className="btcd-paper-inp w-7"
+            disabled={!pipeDriveConf.moduleData?.module}>
+            <option value="">{__('Select Related Module', 'bit-integrations')}</option>
+            {pipeDriveConf.default.modules[pipeDriveConf.moduleData.module]?.relatedlists?.map(
+              (relatedlistApiName) => (
+                <option key={relatedlistApiName.name} value={relatedlistApiName.name}>
+                  {relatedlistApiName.name}
+                </option>
+              )
+            )}
+          </select>
+          <br />
+          <br />
+          {pipeDriveConf.default.modules[pipeDriveConf?.relatedlists?.[tab - 1]?.module]
+            ?.fields && (
+            <>
+              <div className="mt-4">
+                <b className="wdt-100">{__('Field Map', 'bit-integrations')}</b>
+              </div>
+              <div className="btcd-hr mt-1" />
+              <div className="flx flx-around mt-2 mb-2 btcbi-field-map-label">
+                <div className="txt-dp">
+                  <b>{__('Form Fields', 'bit-integrations')}</b>
+                </div>
+                <div className="txt-dp">
+                  <b>{__('PipeDrive Fields', 'bit-integrations')}</b>
+                </div>
+              </div>
+
+              {pipeDriveConf.relatedlists?.[tab - 1]?.field_map?.map((itm, i) => (
+                <PipeDriveFieldMap
+                  key={`crm-m-${i + 9}`}
+                  i={i}
+                  field={itm}
+                  pipeDriveConf={pipeDriveConf}
+                  formFields={formFields}
+                  setPipeDriveConf={setPipeDriveConf}
+                  tab={tab}
+                  setSnackbar={setSnackbar}
+                />
+              ))}
+              <div className="txt-center btcbi-field-map-button mt-2">
+                <button
+                  onClick={() =>
+                    addFieldMap(
+                      pipeDriveConf.relatedlists[tab - 1].field_map.length,
+                      pipeDriveConf,
+                      setPipeDriveConf,
+                      false,
+                      tab
+                    )
+                  }
+                  className="icn-btn sh-sm"
+                  type="button">
+                  +
+                </button>
+              </div>
+              <br />
+              <br />
+              <div className="mt-4">
+                <b className="wdt-100">{__('Utilities', 'bit-integrations')}</b>
+              </div>
+              <div className="btcd-hr mt-1" />
+
+              <PipeDriveActions
+                pipeDriveConf={pipeDriveConf}
+                setPipeDriveConf={setPipeDriveConf}
+                tab={tab}
+                isLoading={isLoading}
+                setIsLoading={setIsLoading}
+                setSnackbar={setSnackbar}
+              />
+            </>
           )}
-        </select>
-        <br />
-        <br />
-      </div>
-      <br />
-      <br />
-      {pipeDriveConf.default.modules[pipeDriveConf?.relatedlists?.[tab - 1]?.module]?.fields && (
-        <>
-          <div className="mt-4">
-            <b className="wdt-100">{__('Field Map', 'bit-integrations')}</b>
-          </div>
-          <div className="btcd-hr mt-1" />
-          <div className="flx flx-around mt-2 mb-2 btcbi-field-map-label">
-            <div className="txt-dp">
-              <b>{__('Form Fields', 'bit-integrations')}</b>
-            </div>
-            <div className="txt-dp">
-              <b>{__('PipeDrive Fields', 'bit-integrations')}</b>
-            </div>
-          </div>
-
-          {pipeDriveConf.relatedlists?.[tab - 1]?.field_map?.map((itm, i) => (
-            <PipeDriveFieldMap
-              key={`crm-m-${i + 9}`}
-              i={i}
-              field={itm}
-              pipeDriveConf={pipeDriveConf}
-              formFields={formFields}
-              setPipeDriveConf={setPipeDriveConf}
-              tab={tab}
-              setSnackbar={setSnackbar}
-            />
-          ))}
-          <div className="txt-center btcbi-field-map-button mt-2">
-            <button
-              onClick={() =>
-                addFieldMap(
-                  pipeDriveConf.relatedlists[tab - 1].field_map.length,
-                  pipeDriveConf,
-                  setPipeDriveConf,
-                  false,
-                  tab
-                )
-              }
-              className="icn-btn sh-sm"
-              type="button">
-              +
-            </button>
-          </div>
-          <br />
-          <br />
-          <div className="mt-4">
-            <b className="wdt-100">{__('Utilities', 'bit-integrations')}</b>
-          </div>
-          <div className="btcd-hr mt-1" />
-
-          <PipeDriveActions
-            pipeDriveConf={pipeDriveConf}
-            setPipeDriveConf={setPipeDriveConf}
-            tab={tab}
-            isLoading={isLoading}
-            setIsLoading={setIsLoading}
-            setSnackbar={setSnackbar}
-          />
-        </>
+        </div>
       )}
+
       {pipeDriveConf.default.modules[pipeDriveConf.relatedlists[tab - 1]?.module] &&
         !pipeDriveConf.default.modules[pipeDriveConf?.relatedlists?.[tab - 1]?.module]?.fields && (
           <Loader
