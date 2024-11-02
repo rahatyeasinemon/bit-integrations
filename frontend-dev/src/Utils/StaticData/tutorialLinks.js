d88417b0e9aea340e14e16162a7672c74818a6bf
Fahim Sakib <fsakib991@gmail.com>
2024-11-02 16:20:35 +0600

    chore: doc link added in tutorialLinks.js

--- frontend-dev/src/Utils/StaticData/tutorialLinks.js
+++ frontend-dev/src/Utils/StaticData/tutorialLinks.js
@@ -565,23 +565,23 @@ const tutorialLinks = {
   },
   newsletter: {
     youTubeLink: '#',
-    docLink: '#'
+    docLink: 'https://bit-integrations.com/wp-docs/actions/newsletter-integrations/'
   },
   sureMembers: {
     youTubeLink: '#',
-    docLink: '#'
+    docLink: 'https://bit-integrations.com/wp-docs/actions/suremembers-integrations-as-an-action/'
   },
   mailster: {
     youTubeLink: '#',
-    docLink: '#'
+    docLink: 'https://bit-integrations.com/wp-docs/actions/mailster-integrations-as-an-action/'
   },
   wpforo: {
     youTubeLink: '#',
-    docLink: 'https://bitapps.pro/docs/bit-integrations/actions/wpforo-integration/'
+    docLink: 'https://bit-integrations.com/wp-docs/actions/wpforo-integrations-as-an-action/'
   },
   dokan: {
     youTubeLink: '#',
-    docLink: '#'
+    docLink: 'https://bit-integrations.com/wp-docs/actions/dokan-integrations-as-an-action/'
   },
   drip: {
     youTubeLink: '#',
@@ -589,15 +589,15 @@ const tutorialLinks = {
   },
   jetEngine: {
     youTubeLink: '#',
-    docLink: '#'
+    docLink: 'https://bit-integrations.com/wp-docs/actions/jetengine-integrations-as-an-action/'
   },
   highLevel: {
     youTubeLink: '#',
-    docLink: '#'
+    docLink: 'https://bit-integrations.com/wp-docs/actions/gohighlevel-integrations/'
   },
   theEventsCalendar: {
     youTubeLink: '#',
-    docLink: '#'
+    docLink: 'https://bit-integrations.com/wp-docs/actions/the-events-calendar-integrations-as-an-action/'
   }
 }
 export default tutorialLinks
