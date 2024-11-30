68315df8321f4ed90d618399b200dadb989c921b
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-11-30 13:13:48 +0600

    remove: unnecessary codes

--- frontend-dev/src/components/AllIntegrations/Telegram/TelegramIntegLayout.jsx
+++ frontend-dev/src/components/AllIntegrations/Telegram/TelegramIntegLayout.jsx
@@ -25,7 +25,6 @@ export default function TelegramIntegLayout({
   }
 
   const setMessageBody = (val) => {
-    console.log(val)
     setTelegramConf(prevConf => create(prevConf, draftConf => {
       draftConf.body = val
     }))
