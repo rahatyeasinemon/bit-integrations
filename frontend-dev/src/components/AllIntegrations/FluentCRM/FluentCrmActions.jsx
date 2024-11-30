b8e4a3be325e275d5b74b73edbc53b4385bddf22
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-11-30 16:11:00 +0600

    feat: FluentCRM assign company pro validation added

--- frontend-dev/src/components/AllIntegrations/FluentCRM/FluentCrmActions.jsx
+++ frontend-dev/src/components/AllIntegrations/FluentCRM/FluentCrmActions.jsx
@@ -7,9 +7,13 @@ import ConfirmModal from '../../Utilities/ConfirmModal'
 import { getAllCompanies } from './FluentCrmCommonFunc'
 import MultiSelect from 'react-multiple-select-dropdown-lite'
 import Loader from '../../Loaders/Loader'
+import { useRecoilValue } from 'recoil'
+import { $btcbi } from '../../../GlobalStates'
 
 export default function FluentCrmActions({ fluentCrmConf, setFluentCrmConf, loading, setLoading, setSnackbar }) {
   const [actionMdl, setActionMdl] = useState({ show: false })
+  const btcbi = useRecoilValue($btcbi)
+  const { isPro } = btcbi
 
   const actionHandler = (e, type) => {
     const newConf = { ...fluentCrmConf }
@@ -65,56 +69,69 @@ export default function FluentCrmActions({ fluentCrmConf, setFluentCrmConf, load
         onChange={(e) => actionHandler(e, 'company_id')}
         className="wdt-200 mt-4 mr-2"
         value="company_id"
-        title={__('Assign Company', 'bit-integrations')}
-        subTitle={__('Assign Company for contact', 'bit-integrations')}
+        isInfo={!isPro}
+        title={`${__('Assign Company', 'bit-integrations')} ${!isPro ? '(Pro)' : ''}`}
+        subTitle={isPro
+          ? __('Assign Company for contact', 'bit-integrations')
+          : sprintf(
+            __(
+              'The Bit Integration Pro v(%s) plugin needs to be installed and activated to enable the %s feature',
+              'bit-integrations'
+            ),
+            '2.3.8',
+            __('Assign Company', 'bit-integrations')
+          )
+        }
       />
 
-      <ConfirmModal
-        className="custom-conf-mdl"
-        mainMdlCls="o-v"
-        btnClass="purple"
-        btnTxt={__('Ok', 'bit-integrations')}
-        show={actionMdl.show === 'company_id'}
-        close={clsActionMdl}
-        action={clsActionMdl}
-        title={__('Assign Company', 'bit-integrations')}>
-        <div className="btcd-hr mt-2 mb-2" />
-        <div className="mt-2">{__('Select Company', 'bit-integrations')}</div>
-        {loading?.company ? (
-          <Loader
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: 45,
-              transform: 'scale(0.5)'
-            }}
-          />
-        ) : (
-          <div className="flx flx-center mt-2">
-            <MultiSelect
-              options={fluentCrmConf?.companies && fluentCrmConf.companies.map(item => ({ label: item.label, value: item.id.toString() }))}
-              className="msl-wrp-options"
-              defaultValue={fluentCrmConf.actions?.company_id?.toString() || ''}
-              onChange={(val) => actionHandler(val, 'company')}
-              singleSelect
-              selectOnClose
-            />
-            <button
-              onClick={() =>
-                getAllCompanies(fluentCrmConf, setFluentCrmConf, loading, setLoading, setSnackbar)
-              }
-              className="icn-btn sh-sm ml-2 mr-2 tooltip"
+      {isPro && (
+        <ConfirmModal
+          className="custom-conf-mdl"
+          mainMdlCls="o-v"
+          btnClass="purple"
+          btnTxt={__('Ok', 'bit-integrations')}
+          show={actionMdl.show === 'company_id'}
+          close={clsActionMdl}
+          action={clsActionMdl}
+          title={__('Assign Company', 'bit-integrations')}>
+          <div className="btcd-hr mt-2 mb-2" />
+          <div className="mt-2">{__('Select Company', 'bit-integrations')}</div>
+          {loading?.company ? (
+            <Loader
               style={{
-                '--tooltip-txt': `'${__('Refresh Companies', 'bit-integrations')}'`
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: 45,
+                transform: 'scale(0.5)'
               }}
-              type="button"
-              disabled={loading?.company}>
-              &#x21BB;
-            </button>
-          </div>
-        )}
-      </ConfirmModal>
+            />
+          ) : (
+            <div className="flx flx-center mt-2">
+              <MultiSelect
+                options={fluentCrmConf?.companies && fluentCrmConf.companies.map(item => ({ label: item.label, value: item.id.toString() }))}
+                className="msl-wrp-options"
+                defaultValue={fluentCrmConf.actions?.company_id?.toString() || ''}
+                onChange={(val) => actionHandler(val, 'company')}
+                singleSelect
+                selectOnClose
+              />
+              <button
+                onClick={() =>
+                  getAllCompanies(fluentCrmConf, setFluentCrmConf, loading, setLoading, setSnackbar)
+                }
+                className="icn-btn sh-sm ml-2 mr-2 tooltip"
+                style={{
+                  '--tooltip-txt': `'${__('Refresh Companies', 'bit-integrations')}'`
+                }}
+                type="button"
+                disabled={loading?.company}>
+                &#x21BB;
+              </button>
+            </div>
+          )}
+        </ConfirmModal>
+      )}
     </div>
   )
 }
